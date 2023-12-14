d0aecaf456bf40c4ca8aacafee8d052d94e110a1
ysungwon <hwoarang09@naver.com>
2023-12-14 17:32:58 +0900

    파일로 커밋하는 중. ejs에 조건문 넣기 성공 ejs에서 특정 값 반복문 수행하면서 pass하기 성공

--- DAY13/nodeviewapp/routes/article.js
+++ DAY13/nodeviewapp/routes/article.js
@@ -3,9 +3,64 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/list", function (req, res, next) {
-  res.render("article/list");
+  const articles = [
+    {
+      article_id: 1,
+      board_type_code: 1,
+      title: "공지게시글1번글",
+      contents: "1번글 내용",
+      view_count: 10,
+      ip_address: "111.111.123.44",
+      is_display_code: 1,
+      reg_date: "2023-12-12",
+      reg_member_id: "eddy",
+    },
+    {
+      article_id: 2,
+      board_type_code: 2,
+      title: "기술블로깅 1번글",
+      contents: "2번글 내용",
+      view_count: 12,
+      ip_address: "111.111.123.42",
+      is_display_code: 1,
+      reg_date: "2023-12-22",
+      reg_member_id: "eddy2",
+    },
+    {
+      article_id: 3,
+      board_type_code: 1,
+      title: "공지게시글3번글",
+      contents: "3번글 내용",
+      view_count: 13,
+      ip_address: "111.111.123.49",
+      is_display_code: 2,
+      reg_date: "2023-12-14",
+      reg_member_id: "eddy3",
+    },
+  ];
+  res.render("article/list", { articles });
 });
 
+router.post("/list", async (req, res) => {
+  var boardTypeCode = req.body.boardTypeCode;
+  var title = req.body.title;
+  var isDisplayCode = req.body.isDisplayCode;
+
+  const articles = [
+    {
+      article_id: 3,
+      board_type_code: 1,
+      title: "공지게시글3번글",
+      contents: "3번글 내용",
+      view_count: 13,
+      ip_address: "111.111.123.49",
+      is_display_code: 2,
+      reg_date: "2023-12-14",
+      reg_member_id: "eddy3",
+    },
+  ];
+  res.render("article/list", { articles });
+});
 router.get("/create", function (req, res, next) {
   res.render("article/create");
 });

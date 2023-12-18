719aea979d65f3dfdc33d4ab707bbc93cd738ca3
ysungwon <hwoarang09@naver.com>
2023-12-18 16:58:30 +0900

    layout

--- DAY13/nodeviewapp/app.js
+++ DAY13/nodeviewapp/app.js
@@ -4,6 +4,9 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+//레이아웃 참조
+var expressLayouts = require("express-ejs-layouts");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var articleRouter = require("./routes/article");
@@ -13,6 +16,13 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+//레이아웃 설정
+app.set("layout", "layout");
+app.set("layout extractScripts", true); //컨텐츠 페이지 내 script 태그를 레이아웃에 통합할 지 여부
+app.set("layout extractStyles", true); //style
+app.set("layout extractMetas", true); //meta -> 난 메타는 연습 안따라감;;나중에 다시 설명해주신다 다 함.
+app.use(expressLayouts);
 
 app.use(logger("dev"));
 app.use(express.json());

4855d071081d5429c2657f4707ab6c9cff5525a4
ysungwon <hwoarang09@naver.com>
2024-01-03 01:39:24 +0900

    23 update

--- DAY23/nodechatapp/routes/channelAPI.js
+++ DAY23/nodechatapp/routes/channelAPI.js
@@ -1,4 +1,4 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,10 +7,10 @@ var channel_list = [
     channel_id: 1,
     community_id: 110011,
     category_code: 1021,
-    channel_name: "채널1",
+    channel_name: '채널1',
     user_limit: 100,
-    channel_img_path: "채널1이미지",
-    channel_desc: "채널1설명",
+    channel_img_path: '채널1이미지',
+    channel_desc: '채널1설명',
     channel_state_code: 1,
     reg_date: Date.now(),
     reg_member_id: 881,
@@ -21,10 +21,10 @@ var channel_list = [
     channel_id: 2,
     community_id: 220022,
     category_code: 1022,
-    channel_name: "채널2",
+    channel_name: '채널2',
     user_limit: 200,
-    channel_img_path: "채널2이미지",
-    channel_desc: "채널2설명",
+    channel_img_path: '채널2이미지',
+    channel_desc: '채널2설명',
     channel_state_code: 0,
     reg_date: Date.now(),
     reg_member_id: 882,
@@ -35,10 +35,10 @@ var channel_list = [
     channel_id: 3,
     community_id: 330033,
     category_code: 1023,
-    channel_name: "채널3",
+    channel_name: '채널3',
     user_limit: 300,
-    channel_img_path: "채널3이미지",
-    channel_desc: "채널3설명",
+    channel_img_path: '채널3이미지',
+    channel_desc: '채널3설명',
     channel_state_code: 1,
     reg_date: Date.now(),
     reg_member_id: 883,
@@ -46,88 +46,175 @@ var channel_list = [
     edit_member_id: 993,
   },
 ];
-router.get("/all", async (req, res, next) => {
-  res.render("channel/list", { channel_list });
+var apiResult = {
+  code: 200,
+  data: [],
+  result: 'OK',
+};
+
+router.get('/all', async (req, res, next) => {
+  try {
+    apiResult.code = 200;
+    apiResult.data = channel_list;
+    apiResult.result = 'OK';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
+
+  res.json(apiResult);
 });
 
-router.post("/create", async (req, res, next) => {
-  var channel_id = req.body.channel_id;
-  var community_id = req.body.community_id;
-  var category_code = req.body.category_code;
-  var channel_name = req.body.channel_name;
-  var user_limit = req.body.user_limit;
-  var channel_img_path = req.body.channel_img_path;
-  var channel_desc = req.body.channel_desc;
-  var channel_state_code = req.body.channel_state_code;
-  var reg_member_id = req.body.reg_member_id;
-  var edit_member_id = req.body.edit_member_id;
+router.post('/create', async (req, res, next) => {
+  try {
+    var channel_id = req.body.channel_id;
+    var community_id = req.body.community_id;
+    var category_code = req.body.category_code;
+    var channel_name = req.body.channel_name;
+    var user_limit = req.body.user_limit;
+    var channel_img_path = req.body.channel_img_path;
+    var channel_desc = req.body.channel_desc;
+    var channel_state_code = req.body.channel_state_code;
+    var reg_member_id = req.body.reg_member_id;
+    var edit_member_id = req.body.edit_member_id;
 
-  var channel = {
-    channel_id,
-    community_id,
-    category_code,
-    channel_name,
-    user_limit,
-    channel_img_path,
-    channel_desc,
-    channel_state_code,
-    reg_date: Date.now(),
-    reg_member_id,
-    edit_date: Date.now(),
-    edit_member_id,
-  };
+    var channel = {
+      channel_id,
+      community_id,
+      category_code,
+      channel_name,
+      user_limit,
+      channel_img_path,
+      channel_desc,
+      channel_state_code,
+      reg_date: Date.now(),
+      reg_member_id,
+      edit_date: Date.now(),
+      edit_member_id,
+    };
 
-  console.log("channel : ", channel);
-  res.send(channel);
+    //
+    var savedChannel = {
+      channel_id: 1,
+      community_id: 110011,
+      category_code: 1021,
+      channel_name: '채널1',
+      user_limit: 100,
+      channel_img_path: '채널1이미지',
+      channel_desc: '채널1설명',
+      channel_state_code: 1,
+      reg_date: Date.now(),
+      reg_member_id: 881,
+      edit_date: Date.now(),
+      edit_member_id: 991,
+    };
+
+    apiResult.code = 200;
+    apiResult.data = savedChannel;
+    apiResult.result = 'OK';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
+
+  res.json(apiResult);
 });
 
-router.post("/modify", async (req, res, next) => {
-  var channel_id = req.params.channel_id;
-  var community_id = req.body.community_id;
-  var category_code = req.body.category_code;
-  var channel_name = req.body.channel_name;
-  var user_limit = req.body.user_limit;
-  var channel_img_path = req.body.channel_img_path;
-  var channel_desc = req.body.channel_desc;
-  var channel_state_code = req.body.channel_state_code;
-  var reg_member_id = req.body.reg_member_id;
-  var edit_member_id = req.body.edit_member_id;
+router.post('/modify', async (req, res, next) => {
+  try {
+    var channel_id = req.body.channel_id;
+    var community_id = req.body.community_id;
+    var category_code = req.body.category_code;
+    var channel_name = req.body.channel_name;
+    var user_limit = req.body.user_limit;
+    var channel_img_path = req.body.channel_img_path;
+    var channel_desc = req.body.channel_desc;
+    var channel_state_code = req.body.channel_state_code;
+    var reg_member_id = req.body.reg_member_id;
+    var edit_member_id = req.body.edit_member_id;
 
-  var channel = {
-    channel_id,
-    community_id,
-    category_code,
-    channel_name,
-    user_limit,
-    channel_img_path,
-    channel_desc,
-    channel_state_code,
-    reg_date: Date.now(),
-    reg_member_id,
-    edit_date: Date.now(),
-    edit_member_id,
-  };
+    var channel = {
+      channel_id,
+      community_id,
+      category_code,
+      channel_name,
+      user_limit,
+      channel_img_path,
+      channel_desc,
+      channel_state_code,
+      reg_date: Date.now(),
+      reg_member_id,
+      edit_date: Date.now(),
+      edit_member_id,
+    };
 
-  console.log("channel modify: ", channel);
+    //
+    var affectedCnt = 1;
 
-  res.send(channel);
+    apiResult.code = 200;
+    apiResult.data = affectedCnt;
+    apiResult.result = 'OK';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
+
+  res.json(apiResult);
 });
 
-router.post("/delete", async (req, res, next) => {
-  var channel_id = req.body.channel_id;
-  console.log("channel_id in delte ", channel_id);
-  res.send({ channel_id });
+router.post('/delete', async (req, res, next) => {
+  try {
+    var channel_id = req.body.channel_id;
+    //삭제
+
+    var affectedCnt = 1;
+
+    apiResult.code = 200;
+    apiResult.data = affectedCnt;
+    apiResult.result = 'OK';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
+
+  res.json(apiResult);
 });
 
-router.get("/:cid", async (req, res, next) => {
-  var cid = req.params.cid;
-  console.log("cid : " + cid);
-  var channel = channel_list.filter((channel) => {
-    console.log("channel_id : " + channel.channel_id);
-    if (channel.channel_id === Number(cid)) return channel;
-  });
-  console.log("channel :" + JSON.stringify(channel, null, 2));
-  //res.render("member/list", { member_list });
-  res.send(channel);
+router.get('/:cid', async (req, res, next) => {
+  try {
+    var cid = req.params.cid;
+    // console.log("cid : " + cid);
+
+    //임시 Data
+    var savedChannel = {
+      channel_id: 3,
+      community_id: 330033,
+      category_code: 1023,
+      channel_name: '채널3',
+      user_limit: 300,
+      channel_img_path: '채널3이미지',
+      channel_desc: '채널3설명',
+      channel_state_code: 1,
+      reg_date: Date.now(),
+      reg_member_id: 883,
+      edit_date: Date.now(),
+      edit_member_id: 993,
+    };
+
+    apiResult.code = 200;
+    apiResult.data = savedChannel;
+    apiResult.result = 'OK';
+  } catch (err) {
+    apiResult.code = 500;
+    apiResult.data = null;
+    apiResult.result = 'Failed';
+  }
+
+  res.json(apiResult);
 });
+
 module.exports = router;
--- DAY23/nodechatapp/routes/index.js
+++ DAY23/nodechatapp/routes/index.js
@@ -1,56 +1,93 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models/index");
+const member = require("../models/member");
 var Op = db.Sequelize.Op;
 
 /* GET home page. */
-//router.get("/", async (req, res) => res.render("login", { layout: true }));
-router.get(["/", "/login"], async (req, res) => {
-  res.render("login", { layout: true });
-});
 
-router.get("/index", async (req, res, next) => {
-  res.render("index", { title: "login", layout: false });
-});
+// router.get("/login", async (req, res) => res.render("login", { layout: true }));
 
-router.post(["/", "/login"], async (req, res, next) => {
-  var searchOption = ({ email, member_password } = req.body);
+router.get("/", async (req, res) =>
+  res.render("login", { resultMsg: "", email: "", password: "", layout: true })
+);
 
-  var member = await db.Member.findOne({
-    attributes: ["email", "member_password"],
-    where: {
-      email: searchOption.email,
-    },
-  });
-  // member = member.map((arr) => {
-  //   return arr.dataValues;
-  // });
-  console.log("searchOption : ", searchOption);
-  console.log("member : ", member);
-  res.redirect("/index");
-});
+// router.get("/index", async (req, res, next) => {
+//   res.render("index", { title: "login", layout: false });
+// });
 
-router.get("/entry", function (req, res, next) {
-  res.render("entry", { title: "entry" });
-});
-
-router.post("/entry", function (req, res, next) {
+router.post("/", async (req, res, next) => {
   let email = req.body.email;
   let password = req.body.password;
-  console.log(`entry post!! email : ${email}   password : ${password}`);
+
+  let member = await db.Member.findOne({ where: { email } });
+
+  //로그인 로직 구현
+  resultMsg = "";
+
+  if (member == null) {
+    resultMsg = "이메일이 일치하지 않습니다.";
+  } else {
+    if (member.member_password == password) {
+      res.redirect("/chat");
+    } else {
+      resultMsg = "비밀번호가 일치하지 않습니다.";
+    }
+  }
+
+  if (resultMsg != "") {
+    res.render("login", { resultMsg, email, password });
+  }
+});
+
+router.get("/entry", async (req, res, next) => {
+  res.render("entry");
+});
+
+router.post("/entry", async (req, res, next) => {
+  let email = req.body.email;
+  let name = req.body.name;
+  let member_password = req.body.member_password;
+  let confirm_member_password = req.body.confirm_member_password;
+  let telephone = req.body.telephone;
+  let birth_date = req.body.birth_date;
+  let entry_type_code = req.body.entry_type_code;
+
+  let member = {
+    email,
+    member_password,
+    confirm_member_password,
+    name,
+    profile_img_path: 1,
+    telephone,
+    entry_type_code,
+    use_state_code: 1,
+    birth_date,
+    reg_date: Date.now(),
+    reg_member_id: 1,
+  };
+
+  let savedMember = await db.Member.create(member);
 
   res.redirect("/");
 });
 
-router.get("/find", function (req, res, next) {
-  res.render("find", { title: "find" });
+router.get("/find", async (req, res, next) => {
+  res.render("find", { resultMsg: "", email: "" });
 });
 
-router.post("/find", function (req, res, next) {
+router.post("/find", async (req, res, next) => {
   let email = req.body.email;
-  let password = req.body.password;
-  console.log(`find post!! email : ${email}   password : ${password}`);
 
-  res.redirect("/");
+  let findEmail = await db.Member.findOne({ where: { email } });
+
+  resultMsg = "";
+
+  if (findEmail === null) {
+    resultMsg = "존재하지 않는 이메일입니다.";
+    res.render("find", { resultMsg, email });
+  } else {
+    res.redirect("/");
+  }
 });
 module.exports = router;

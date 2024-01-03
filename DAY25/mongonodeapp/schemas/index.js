8e39357d9df4574cb40af0c0e534299cd72aa13f
ysungwon <hwoarang09@naver.com>
2024-01-03 12:38:03 +0900

    day25

---
+++ DAY25/mongonodeapp/schemas/index.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connect = async function connectToMongoDB() {
+  try {
+    await mongoose.connect("mongodb://eddy:eddy524640!@localhost:27017/", {
+      dbName: "modu_chat",
+    });
+    console.log("Connected to MongoDB");
+    // Your code here
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = connect;

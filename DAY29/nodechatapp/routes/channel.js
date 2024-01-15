6b9bee9961503485929de2d6f4af597651c383a5
ysungwon <hwoarang09@naver.com>
2024-01-16 01:56:37 +0900

    hi

--- DAY29/nodechatapp/routes/channel.js
+++ DAY29/nodechatapp/routes/channel.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { tokenAuthChecking } = require("./apiMiddleware");
 /* GET home page. */
-router.get("/", async (req, res, next) => {
+router.get("/", tokenAuthChecking, async (req, res, next) => {
   console.log("chat start!!!!");
   res.render("chat/index", { layout: "layout/chatLayout" });
 });
 
-router.get("/verify", tokenAuthChecking, async (req, res, next) => {
+router.post("/verify", tokenAuthChecking, async (req, res, next) => {
+  console.log("chat verify!");
   res.json({ code: "200", message: "Chat started" });
 });
 module.exports = router;
